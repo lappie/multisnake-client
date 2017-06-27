0ca09371d25e1939e6aee811989e74e08989ef5a
J Lappie <lappi@lappietop>
2017-06-27 22:09:33 +0200

    small fixes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-let wsUri = "ws://echo.websocket.org/";
+let wsUri = "ws://localhost:9000";
 let websocket = new WebSocket(wsUri);
+
+let userid = 1;
 
 websocket.onmessage = function(evt) { console.log('MESSAGE: ' + evt.data) };
 
-function left() {
-	console.log('left');
-	websocket.send("left");
+function send(msg) {
+	websocket.send(userid + "/" + msg);
 }
