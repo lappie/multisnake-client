c41b8f700e778a82028bcf69ede8865d68d6c0fa
J Lappie <lappi@lappietop>
2017-07-16 13:03:03 +0200

    Added registration. Added auto reconnect module after max 5s

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-let wsUri = "ws://localhost:9000";
-let websocket = new WebSocket(wsUri);
+let wsUri = "ws://localhost:9001";
+let websocket = null;
 
 let userid = 1;
+let connected = false;
 
-websocket.onmessage = function(evt) { console.log('MESSAGE: ' + evt.data) };
-websocket.onclose = function(evt) { document.getElementById('closed').style.display = 'block'};
+function start() {
+	websocket = new WebSocket(wsUri);
+
+	websocket.onmessage = function(evt) { console.log('MESSAGE: ' + evt.data) };
+	websocket.onclose = function(evt) {
+		connected = false;
+		document.getElementById('closed').style.display = 'block';
+		window.setTimeout(start, 5000);
+	};
+	websocket.onopen = function(evt) {
+		connected = true;
+		document.getElementById('closed').style.display = 'none';
+		document.getElementById('register-screen').style.display = 'block';
+	}
+}
+start();
 
 function send(msg) {
-	websocket.send(userid + "/" + msg);
+	if(connected)
+		websocket.send(userid + "/" + msg);
 }
 
 function buttonAnimation(el) {
@@ -16,6 +32,15 @@ function buttonAnimation(el) {
 
 function resetAnimation(el) {
 	el.style.backgroundColor = "white";
+}
+
+function register() {
+	let playername = document.getElementById("player-name").value;
+	if(playername.length == 0)
+		return;
+	userid = playername;
+	send("register");
+	document.getElementById('register-screen').style.display = 'none';
 }
 
 var body = document.getElementById("app");
